d8b722fadad0e451e63adfcac1d92b3451317b22
Michael <jagosikat@yahoo.co.id>
2019-05-17 15:18:47 +0700

    hoho

--- testAnimation/ViewController.swift
+++ testAnimation/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Green: UIView!
+    @IBOutlet weak var blueBox: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UIView.animate(withDuration: 1, animations:{
+            self.blcueBox.frame  = CGRect(x: 0, y: 0, width: 100, height: 100)
+            self.Green.bounds  = CGRect(x: 0, y: 0, width: 100, height: 100)
+            
+        })
     }
 
 
